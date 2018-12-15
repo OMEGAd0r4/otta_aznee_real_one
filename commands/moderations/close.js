cff968e12d86f1e169619c3606d97fcf69edfbe4
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-15 11:50:20 +0700

    Create close.js

---
+++ commands/moderations/close.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = "/";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+const ms = require("ms");
+
+class closeCommand extends commando.Command {
+    constructor(client) 
+    {
+      super(client, {
+        name: 'close', 
+        group: 'moderations',
+        memberName: 'close',
+        description: "Closes a support ticket"
+      });
+    }
+
+    async run(message, args)
+    {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`/confirm\`. This will time out in 20 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '/confirm', {
+        max: 1,
+        time: 20000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+    }
+}
+
+module.exports = closeCommand;
