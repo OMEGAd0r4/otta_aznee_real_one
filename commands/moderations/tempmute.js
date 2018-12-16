f840e37c41c878e2bd20c610e1861bc2fa9c6f65
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-16 15:59:51 +0700

    Update tempmute.js

--- commands/moderations/tempmute.js
+++ commands/moderations/tempmute.js
@@ -19,68 +19,46 @@ class tempmuteCommand extends commando.Command {
 
     async run(message, args)
     {
-        let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!tomute) return message.reply("Couldn't find user.");
+        var tempmuteargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+        var tempmuteuser = message.guild.member(message.mentions.users.first() || message.guild.members.get(tempmuteargs[0]));
+        var tempmutereason = tempmuteargs.join(" ").slice(22);
+        var tempmuterole = message.guild.roles.find(`name`, "Temp-Muted");
+
+        if (!tempmuterole) return message.channel.send("Please make a 'Temp-Muted' role to proceed")
+
+        if (!tempmuteuser) return message.channel.send({embed: new Discord.RichEmbed()
+            .setDescription(":x: **Missing args**")
+            .setColor("#FF4040")
+            .addField("->", "/tempmute [User] [Reason]")});
+
+        if (!tempmutereason) return message.channel.send({embed: new Discord.RichEmbed()
+            .setDescription(":x: **Missing args**")
+            .setColor("#FF4040")
+            .addField("->", "/tempmute [User] [Reason]")});
+
         if (!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.channel.send("Insufficient permission. You do not have permission to mute others")
-        if (tomute.hasPermission("VIEW_AUDIT_LOG")) return message.channel.send("Insufficient permission. You do not have permission to mute admins!")
-        let muterole = message.guild.roles.find(`name`, "Temp-Muted");
-        //start of create role
-        if(!muterole){
-          try{
-            muterole = await message.guild.createRole({
-              name: "Temp-Muted",
-              color: "#000000",
-              permissions:[]
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-              await channel.overwritePermissions(muterole, {
-                SEND_MESSAGES: false,
-                ADD_REACTIONS: false
-              });
-            });
-          }catch(e){
-            console.log(e.stack);
-          }
-        }
-        //end of create role
-        let mutetime = args[1];
-        if(!mutetime) return message.channel.send({embed: new Discord.RichEmbed()
-            .setDescription(":x: **Missing args**")
-            .setColor("#FF4040")
-            .addField("->", "/tempmute [User] [Reason]")});
 
-        var mutereason = args [2];
-        if (!mutereason) return message.channel.send({embed: new Discord.RichEmbed()
-            .setDescription(":x: **Missing args**")
-            .setColor("#FF4040")
-            .addField("->", "/tempmute [User] [Reason]")});
-      
-        await(tomute.addRole(muterole.id));
-        message.channel.send(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+        if (tempmuteuser.hasPermission("VIEW_AUDIT_LOG")) return message.channel.send("Insufficient permission. You do not have permission to mute admins!")
 
-        var muteembed = new Discord.RichEmbed()
+            var tempmuteembed = new Discord.RichEmbed()
                 .setColor("#2AC940")
                 .setDescription('✅**Temp-Muted**')
-                .addField('**__Temp-Muted User__**', `${tomute} with the ID: ${tomute.id}`)
+                .addField('**__Temp-Muted User__**', `${tempmuteuser} with the ID: ${tempmuteuser.id}`)
                 .addField('**__Temp-Muted By__**', `${message.author} with the ID: ${message.author.id}`)
-                .addField('**__Temp-Muted Reason__**', mutereason)
-                .addField("**__Temp-Mute Duration__**", mutetime)
+                .addField('**__Temp-Muted Reason__**', tempmutereason)
                 .addField('**__Temp-Muted Location__**', message.channel)
                 .addField('**__Temp-Muted Time__**', message.createdAt)
 
                 let logschannel = message.guild.channels.find(`name`, "logs");
                 if(!logschannel) return message.channel.send("Couldn't find the logs channel");
 
-                logschannel.send(muteembed).then(tomute.addRole(muterole.id));
+                logschannel.send(tempmuteembed).then(tempmuteuser.addRole(tempmuterole.id));
 
                 message.delete();
 
-                tomute.sendMessage(tomute + " Hey you have been temp-muted from the Alpha Network for the reason of " + '[' + mutereason + ']')
-      
-        setTimeout(function(){
-          tomute.removeRole(muterole.id);
-          message.channel.send(`<@${tomute.id}> has been unmuted!`);
-        }, ms(mutetime));
+                message.channel.send(`${tempmuteuser} has been temp-muted`)
+
+                tempmuteuser.sendMessage(tempmuteuser + " Hey you have been temp-muted from the Alpha Network for the reason of " + '[' + tempmutereason + ']')
         
     }
 }
