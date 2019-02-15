130f75246bb2f4e2bccc6bfa352dd84f0524b4a8
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2019-02-15 21:56:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.login(process.env.token);
 //GETS THE BOT ONLINE
 bot.on('ready',function(){
     console.log(`Bot is now online!, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
-    bot.user.setActivity(`/help | Helping ${bot.users.size} users, in ${bot.guilds.size} servers`, { type: 'PLAYING' });
+    bot.user.setActivity(`/help | Slender Man`, { type: 'PLAYING' });
 });
 
 bot.on("guildCreate", guild => {
